e9de924fed6ad605e01462ed77d286f6a64bdd4a
Ketan K <keshava.ke@northeastern.edu>
2024-02-07 09:43:49 -0500

    Refactored code: Separated logic for validation

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { inputValidation } from '../middleware/inputValidation.js';
+import { createUserInputValidator } from '../validators/createUserInputValidator.js';
 import { checkExistingUsername } from '../middleware/checkExistingUsername.js';
 import { createUserAccount } from '../controllers/userController.js';
 import { checkNoNoQueryParams} from '../middleware/checkNoQueryParams.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/', checkNoNoQueryParams, inputValidation, checkExistingUsername, createUserAccount);
+userRouter.post('/', checkNoNoQueryParams, createUserInputValidator, checkExistingUsername, createUserAccount);
 
 export default userRouter;
