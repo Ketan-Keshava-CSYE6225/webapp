5c561890df7c5cd3db77d9e09d0e6ca99f422a55
Ketan K <keshava.ke@northeastern.edu>
2024-02-21 18:35:10 -0500

    Passing variables to Shell via GitHub Secrets

--- shell/script_04_setup_db_user.sh
+++ shell/script_04_setup_db_user.sh
@@ -1,17 +1,6 @@
-#!/bin/bash
-
-# Go to Webapp Directory
-cd /opt/csye6225/webapp/
-
-# Log env file
-cat .env || { echo "No .env file found to log"; exit 1;}
-
-# Load env
-source .env
-
 # Set up db & user
-sudo -u postgres psql -c "CREATE DATABASE app_db;"
-sudo -u postgres psql -c "CREATE USER app_user WITH PASSWORD 'password';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE app_db TO app_user;"
+sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
+sudo -u postgres psql -c "CREATE USER $DB_USERNAME WITH PASSWORD '$DB_PASSWORD';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USERNAME;"
 sudo sed -i.bak 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf || { echo "Failed to update pg_hba.conf"; exit 1;}
 sudo systemctl restart postgresql
