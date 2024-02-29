f56aa1f970d7a4be2bdafc4e62f474933f10613c
Ketan K <keshava.ke@northeastern.edu>
2024-02-29 09:33:20 -0500

    Assignment 5 dev

--- shell/script_04_setup_db_user.sh
+++ shell/script_04_setup_db_user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set up db & user
-sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
-sudo -u postgres psql -c "CREATE USER $DB_USERNAME WITH PASSWORD '$DB_PASSWORD';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USERNAME;"
-sudo sed -i.bak 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf || { echo "Failed to update pg_hba.conf"; exit 1;}
-sudo systemctl restart postgresql
+# sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
+# sudo -u postgres psql -c "CREATE USER $DB_USERNAME WITH PASSWORD '$DB_PASSWORD';"
+# sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USERNAME;"
+# sudo sed -i.bak 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf || { echo "Failed to update pg_hba.conf"; exit 1;}
+# sudo systemctl restart postgresql
