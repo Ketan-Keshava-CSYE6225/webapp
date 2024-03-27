4565be0839ec191f37a325367d7b609672cec474
Ketan K <keshava.ke@northeastern.edu>
2024-03-27 11:34:52 -0400

    Added appropriate logs for healthz controller

--- src/controllers/healthController.js
+++ src/controllers/healthController.js
@@ -1,10 +1,13 @@
 import db from '../orm/sequelize.js';
+import logger from '../utils/logger.js';
 
 export const healthController = async (req, res) => {
   try {
     await db.sequelize.authenticate();
+    logger.info('Connection has been established successfully.');
     res.status(200).send();
   } catch (error) {
+    logger.error('Unable to connect to the database:', error);
     res.status(503).send();
   }
 };
